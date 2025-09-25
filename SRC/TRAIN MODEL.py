bfa69e393bad5f14c5dcbecc9aa34c3f0737f8ad
HARISH KUMAR V <231501057@rajalakshmi.edu.in>
2025-09-25 11:44:01 +0530

    Create TRAIN MODEL.py

---
+++ SRC/TRAIN MODEL.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib
+
+# Load dataset
+data = pd.read_csv("../data/stress_data.csv")
+
+X = data[["Load", "Area", "Material_Strength"]]
+y = data["Status"].map({"Safe": 0, "Unsafe": 1})  # Encode labels
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Accuracy
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+# Save model
+joblib.dump(model, "../src/stress_model.pkl")
